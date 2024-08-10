6c4cd0158562484dd14cf90222ec03d5e236c547
Jon Roig <jon@jonroig.com>
2024-08-10 11:19:25 -0700

    es6

--- sqlite/create.sql
+++ sqlite/create.sql
@@ -1,3 +1,6 @@
 CREATE TABLE usNameData ( id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, sex TEXT NOT NULL, births INTEGER NOT NULL, rank INTEGER NOT NULL, year INTEGER NOT NULL );
+CREATE INDEX idx_name ON usNameData(name);
+
 
 CREATE TABLE nameMeanings ( id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, sex TEXT NOT NULL, nameData JSON );
+CREATE INDEX idx_name_sex ON nameMeanings(name, sex);
