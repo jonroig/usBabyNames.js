6c33279a9cafb3356431879c3c89910a83f046eb
Jon Roig <jon@jonroig.com>
2024-08-10 18:41:58 -0700

    better detail handling

--- examples/getMeaning.js
+++
@@ -1,7 +0,0 @@
-const getMeaning = async () => {
-	const usBabyNames = await import('../lib/usBabyNames.mjs');
-	const theMeaning = await usBabyNames.getMeaning('bertha', 'F');
-	console.log('theMeaning', theMeaning);
-  };
-  
-  getMeaning();
