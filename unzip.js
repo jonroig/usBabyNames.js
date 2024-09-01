9686acd783911484d3bad73907f3319a337f7d74
Jon Roig <jon@jonroig.com>
2024-08-31 17:32:46 -0700

    spacing saving

--- unzip.js
+++ unzip.js
@@ -7,14 +7,28 @@ const __dirname = dirname(__filename);
 
 console.log('Unpacking baby names...');
 
-const zipFilePath = join(__dirname, 'sqlite', 'state-name-data.sqlite.zip');
+const stateNameZipFilePath = join(__dirname, 'sqlite', 'state-name-data.sqlite.zip');
+const usNameZipFilePath = join(__dirname, 'sqlite', 'us-name-data.sqlite.zip');
+const usNameDetailsZipFilePath = join(__dirname, 'sqlite', 'us-name-details.sqlite.zip');
+
 const outputDir = join(__dirname, 'sqlite');
 
 const unzip = async () => {
   const { default: AdmZip } = await import('adm-zip');
-  const zip = new AdmZip(zipFilePath);
-  zip.extractAllTo(outputDir, true);
-  console.log('...baby names unpacked');
+
+  const usNameDetailsZip = new AdmZip(usNameDetailsZipFilePath);
+  usNameDetailsZip.extractAllTo(outputDir, true);
+  console.log('...usNameDetailsZip unpacked');
+
+  const stateNameZip = new AdmZip(stateNameZipFilePath);
+  stateNameZip.extractAllTo(outputDir, true);
+  console.log('...stateNameZip unpacked');
+
+  const usNameZip = new AdmZip(usNameZipFilePath);
+  usNameZip.extractAllTo(outputDir, true);
+  console.log('...usNameZip unpacked');
+
+  console.log('\n\nus baby names unpacked!\n\n');
 };
 
 unzip().catch(console.error);
